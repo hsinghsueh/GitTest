e8466c3e21f85a29ceef9d83cf96ff61880b8ceb
James Lu <james0523@hotmail.com>
2012-02-16 02:40:49 +0800

    Signed-off-by: James Lu <james0523@hotmail.com>

--- GitTest/src/GitTest.java
+++ GitTest/src/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git!!");
+		System.out.println("Hello James!!");
 	}
 
 }
